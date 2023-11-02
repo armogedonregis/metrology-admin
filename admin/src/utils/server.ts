b32ed2e6e9c39c955e604f2deb688bbd7d375903
armogedonregis <armag171691@gmail.com>
2023-11-02 19:24:24 +0300

    check  api

--- admin/src/utils/server.ts
+++ admin/src/utils/server.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const isServerForService = dev ? "http://localhost:4000" : "";
+export const isServerForService = dev ? "http://localhost:4000" : "http://45.9.41.151:4000";
